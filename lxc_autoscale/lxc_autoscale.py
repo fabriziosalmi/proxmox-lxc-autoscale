69b76ec9d4ff56b9b536b487e27ca9f51124cd43
fab <fabrizio.salmi@gmail.com>
2024-08-19 13:04:01 +0200

    Update lxc_autoscale.py

--- lxc_autoscale/lxc_autoscale.py
+++ lxc_autoscale/lxc_autoscale.py
@@ -45,7 +45,7 @@ PROXMOX_HOSTNAME = gethostname()
 # Set up logging
 logging.basicConfig(
     filename=LOG_FILE,
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
