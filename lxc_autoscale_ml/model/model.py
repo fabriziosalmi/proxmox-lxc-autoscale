0050261213657809068ca531c1a37b93f8f4420f
fab <fabrizio.salmi@gmail.com>
2024-08-20 12:53:23 +0200

    Update model.py
    - model updated
    - confidence score added
    - max ram/cpu options added

--- lxc_autoscale_ml/model/model.py
+++ lxc_autoscale_ml/model/model.py
@@ -3,12 +3,14 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 import logging
 import numpy as np
-
+import pandas as pd
 
 def train_anomaly_models(df, config):
     # Select only numeric features for training, excluding non-relevant columns
     features_to_use = df.select_dtypes(include=[np.number]).columns.difference(['container_id', 'timestamp'])
     X_train = df[features_to_use]
+
+    logging.info(f"Features used for training: {list(features_to_use)}")
 
     model = IsolationForest(
         contamination=config.get('model', {}).get('contamination', 0.05),
@@ -29,13 +31,23 @@ def train_anomaly_models(df, config):
         logging.error(f"Error during model training: {e}")
         return None, None
 
+
 def predict_anomalies(model, latest_metrics, features_to_use, config):
     # Ensure only the relevant features are used for prediction
     latest_metrics_df = latest_metrics[features_to_use].to_frame().T
 
+    logging.debug(f"Features used for prediction: {latest_metrics_df.columns.tolist()}")
+
     try:
+        anomaly_score = model.decision_function(latest_metrics_df)
+        # Convert anomaly_score to a scalar if it's an array
+        anomaly_score = anomaly_score.item() if isinstance(anomaly_score, np.ndarray) else anomaly_score
+        # Normalize the anomaly score to a confidence level (0 to 100%)
+        confidence = (1 - anomaly_score) * 100  # Inverse because lower scores mean more abnormal
+        logging.debug(f"Anomaly score: {anomaly_score}, Confidence: {confidence:.2f}%")
         prediction = model.predict(latest_metrics_df)
-        return prediction[0]
+        prediction = prediction.item() if isinstance(prediction, np.ndarray) else prediction
+        return prediction, confidence
     except Exception as e:
         logging.error(f"Error during prediction with IsolationForest: {e}")
-        return None
+        return None, 0
